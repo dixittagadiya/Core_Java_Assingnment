9eb6616d1e213e3f3d7492893dcc9332aeb8d6d6
dixittagadiya <139606709+dixittagadiya@users.noreply.github.com>
2024-01-19 13:39:29 +0530

    Update A002_Vowel_Consonant.java

--- A002_Vowel_Consonant.java
+++ A002_Vowel_Consonant.java
@@ -2,30 +2,31 @@ package Modul_2_core_java;
 
 import java.util.Scanner;
 
-public class A002_Vowel_Consonant 
-{
+public class A002_Vowel_Consonant {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter No :");
-		String ch =sc.next();
-		 
-		
-		
-		if(ch == "A"||ch=="E"||ch=="I"||ch=="O"||ch=="U"||ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u")
+		char ch = sc.next().charAt(0);
+
+		if (Character.isDigit(ch)) 
 		{
-			System.out.println(ch+ " is "+" Vowel :");
-		}
-		else if (! ch.matches("[a-zA-Z]") && ch.length() >=1)
-		{
-			System.out.println("Error");
+			System.out.println("Digit..");
 		}
 		
-		else
-		{
-			System.out.println(ch+ " is "+" consonant");
+		else {
+
+			if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u')
+			{
+				System.out.println(ch + " is " + " Vowel :");
+			}
+			else
+			{
+				System.out.println(ch + " is " + " consonant");
+			}
+
 		}
 	}
 
